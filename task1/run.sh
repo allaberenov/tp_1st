4ccbf892f0cbc7d20969157a4352eae6072ce1b9
kerim <allaberenov.kerim.21@gmail.com>
2022-03-02 21:14:35 +0300

    Echo command declined

--- task1/run.sh
+++ task1/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #echo "$1 $2 $3 $4 $5 $6 $7 $8"
 mkdir "$6"
-find $2/ -name "*.$4" -exec cp {} "$6" \; 
+find $2 -name "*.$4" -exec cp {} "$6" \; 
 ls
  tar -cvf $8.gz "$6"
- #echo "Done"
+ #echo "done"
 
