21de48bca4d0fe95f993f0f88245f7bce1bf3cda
kerim <allaberenov.kerim.21@gmail.com>
2022-03-02 21:37:01 +0300

    Trying 1

--- task1/run.sh
+++ task1/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#echo "$1 $2 $3 $4 $5 $6 $7 $8"
-mkdir "$6"
-find $2 -name "*.$4" -exec cp {} "$6" \; 
-ls
- tar -cvf $8.gz "$6"
- #echo "done"
+mkdir $6
+find $2/ -name "*.$4" -exec cp {} "$6" \; 
+tar -cf $8.gz "$6"
+echo "done"
 
