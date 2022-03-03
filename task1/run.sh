dff19603282985ddb16b97a5fdb03854725052ef
kerim <allaberenov.kerim.21@gmail.com>
2022-03-03 06:51:13 +0300

    Try 6

--- task1/run.sh
+++ task1/run.sh
@@ -1,6 +1,88 @@
 #!/bin/bash
-mkdir $6
-find $4 -name "*.$2" -exec cp {} "$6" \; 
-tar -zcf $8.tar.gz "$6"
-echo "done"
 
+intput_folder=0
+extension=0
+backup_folder=0
+backup_arch=0
+
+
+
+case $1 in
+
+  --input_folder)
+    input_folder=$2
+    ;;
+
+  --extension)
+    extension=$2
+    ;;
+
+  --backup_folder)
+    backup_folder=$2
+    ;;
+    
+    --backup_archive_name)
+    backup_arch=$2
+esac
+
+case $3 in
+
+  --input_folder)
+    input_folder=$4
+    ;;
+
+  --extension)
+    extension=$4
+    ;;
+
+  --backup_folder)
+    backup_folder=$4
+    ;;
+    
+    --backup_archive_name)
+    backup_arch=$3
+esac
+
+case $5 in
+
+  --input_folder)
+    input_folder=$6
+    ;;
+
+  --extension)
+    extension=$6
+    ;;
+
+  --backup_folder)
+    backup_folder=$6
+    ;;
+    
+    --backup_archive_name)
+    backup_arch=$6
+esac
+
+case $7 in
+
+  --input_folder)
+    input_folder=$8
+    ;;
+
+  --extension)
+    extension=$8
+    ;;
+
+  --backup_folder)
+    backup_folder=$8
+    ;;
+    
+    --backup_archive_name)
+    backup_arch=$8
+esac
+
+#echo "$input_folder $extension $backup_folder $backup_arch"
+
+
+mkdir $backup_folder
+find $input_folder -name "*.$extension" -exec cp {} "$backup_folder" \; 
+tar -zcf $backup_arch "$backup_folder"
+echo "done"
