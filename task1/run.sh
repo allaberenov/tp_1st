b1c8d1e4b551cc53d3d38551af4c9b2c2689b24c
kerim <allaberenov.kerim.21@gmail.com>
2022-03-07 23:03:24 +0300

    Исправлены замечания

--- task1/run.sh
+++ task1/run.sh
@@ -1,87 +1,36 @@
 #!/bin/bash
 
-#!/bin/bash
-
 intput_folder=0
 extension=0
 backup_folder=0
 backup_arch=0
 
 
+while (( $# )); do
 
-case $1 in
+	echo $1
+	case $1 in
+	  --input_folder)
+	  input_folder=$2
+	    	;;
 
-  --input_folder)
-    input_folder=$2
-    ;;
+	  --extension)
+	  extension=$2
+		;;
 
-  --extension)
-    extension=$2
-    ;;
-
-  --backup_folder)
-    backup_folder=$2
-    ;;
+	  --backup_folder)
+	  backup_folder=$2
+	    	;;
     
-    --backup_archive_name)
-    backup_arch=$2
-esac
+	  --backup_archive_name)
+	  backup_archive=$2
+	esac
+	shift
+	shift
+done;
 
-case $3 in
-
-  --input_folder)
-    input_folder=$4
-    ;;
-
-  --extension)
-    extension=$4
-    ;;
-
-  --backup_folder)
-    backup_folder=$4
-    ;;
-    
-    --backup_archive_name)
-    backup_arch=$3
-esac
-
-case $5 in
-
-  --input_folder)
-    input_folder=$6
-    ;;
-
-  --extension)
-    extension=$6
-    ;;
-
-  --backup_folder)
-    backup_folder=$6
-    ;;
-    
-    --backup_archive_name)
-    backup_arch=$6
-esac
-
-case $7 in
-
-  --input_folder)
-    input_folder=$8
-    ;;
-
-  --extension)
-    extension=$8
-    ;;
-
-  --backup_folder)
-    backup_folder=$8
-    ;;
-    
-    --backup_archive_name)
-    backup_arch=$8
-esac
-
-cp -r $input_folder/ $backup_folder/
-find $backup_folder -type f -not -name "*.$extension" -exec rm {} \;
-tar -zcf $backup_arch "$backup_folder"
+cp -r /$input_folder/ $backup_folder/
+find $backup_folder -type f -not -name "*.$extension" -exec sudo rmz {} \;
+find $input_folder -type d -empty -delete
+tar -zcf $backup_archive "$backup_folder"
 echo "done"
