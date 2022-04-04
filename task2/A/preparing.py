f1df833b7bc90ca52e5d23e349223666a7e2b865
kerim <allaberenov.kerim.21@gmail.com>
2022-04-04 19:45:56 +0300

    удаление уже собранных файлов

--- task2/A/preparing.py
+++ task2/A/preparing.py
@@ -1,5 +1,7 @@
+#pragma once
 
 import sys
 f = open(sys.argv[1]+"/A/index.h", "w+")
-f.write("const int a=2;")
+f.write('''#pragma once
+const int a=2;''')
 f.close()
--- task2/C/main.h
+++ task2/C/main.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "A/index.h"
 #include "B/lib.h"
 
