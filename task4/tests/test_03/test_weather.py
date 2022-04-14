5c6773f7ef5b7bc98c368fdd3a81a136f4600a57
kerim <allaberenov.kerim.21@gmail.com>
2022-04-15 01:32:09 +0300

    Третий модуль протестирован

--- task4/tests/test_03/test_weather.py
+++ task4/tests/test_03/test_weather.py
@@ -1,6 +1,28 @@
 from weather_03 import weather_wrapper
+
 import pytest
+import requests_mock
 import os
+import requests
+
+
+APPID = "edd66adbb181089cb7a6be4fd0329c15"  # <-- Put your OpenWeatherMap appid here!
+URL_BASE = "http://api.openweathermap.org/data/2.5/"
+
 
 def test_add():
-    weather_wrapper.WeatherWrapper('http://api.openweathermap.org/data/2.5/weather')
+    weather = weather_wrapper.WeatherWrapper(APPID)
+
+    with pytest.raises(AttributeError):
+        weather.get_response_city("Turgistan", weather_wrapper.BASE_URL)
+
+    with requests_mock.Mocker(real_http=True) as moc:
+        moc.register_uri('GET', URL_BASE, text='resp')
+        weather.get_tomorrow_temperature("London")
+        weather.find_diff_two_cities("Ashgabat", "London")
+        weather.get_diff_string("Ashgabat", "London")
+        weather.get_diff_string("Moscow", "London")
+        weather.get_tomorrow_diff("Ashgabat")
+        weather.get_tomorrow_diff("London")
+        weather.get_tomorrow_diff("Sankt Peterburg")
+        weather.get_tomorrow_diff("Iceland")
